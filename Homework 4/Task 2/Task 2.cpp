dd7a46beedcf7a3cc1d9b92b8234fb0f90918a03
Gergana Ivanova <geri7423@abv.bg>
2024-03-10 23:34:03 +0200

    task 2

---
+++ Homework 4/Task 2/Task 2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+using namespace std;
+int main()
+{
+    vector<int>numbers;
+    int n;
+    while (true)
+    {
+        cin >> n;
+        if (n == 0) break;
+        numbers.push_back(n);
+    }
+
+    for (vector<int>::iterator p = numbers.begin(); p != numbers.end()-1; p++)
+    {
+        if (*p == *(p + 1))
+        {
+            cout << *p << " ";
+        }
+    }
+}
