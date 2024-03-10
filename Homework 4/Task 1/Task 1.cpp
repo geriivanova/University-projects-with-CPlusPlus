b1ef38d0d46364898a9ee796f9821f3a46ae8e05
Gergana Ivanova <geri7423@abv.bg>
2024-03-10 23:33:29 +0200

    new task

---
+++ Homework 4/Task 1/Task 1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<fstream>
+using namespace std;
+int main()
+{
+	vector <string> words;
+	fstream file;
+	file.open("d:\\hw\\words.txt", ios::in);
+	if (!file.is_open()) {
+		cout << "Error to open file\n";
+		return -1;
+	}
+	
+	string word;
+	while (!file.eof())
+	{
+		getline(file, word);
+		words.push_back(word);
+	}
+	file.close();
+
+	fstream file1;
+	file1.open("d:\\hw\\newfile.txt", ios::out);
+
+	sort(words.begin(), words.end());
+
+	for(int i=0; i<words.size();i++)
+	{
+		file1 << words[i] << endl;
+    }
+	file1.close();
+
+}
+
+
