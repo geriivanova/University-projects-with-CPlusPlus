82ac4a744235bbca9850564a1d7da011582907a3
Gergana Ivanova <geri7423@abv.bg>
2025-05-02 13:13:06 +0300

    solution for task 7

---
+++ SoftUniBasics/FirstSteps/07. ProjectsCreation/07. ProjectsCreation.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	int projectsCount;
+	int hours;
+
+	cin >> name;
+	cin >> projectsCount;
+
+	hours = projectsCount * 3;
+	cout << "The architect " << name << " will need " << hours << " hours to complete " << projectsCount << " project/s." << endl;
+	return 0;
+}
+
