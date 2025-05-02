f756287445fbb20c2d66214ad612162cda124e03
Gergana Ivanova <geri7423@abv.bg>
2025-05-02 13:26:20 +0300

    solution for task 9

---
+++ SoftUniBasics/FirstSteps/09. YardGreening/09. YardGreening.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	double quadrature, price, finalPrice, discount;
+	cin >> quadrature;
+
+	price = quadrature * 7.61;
+	discount = price * 0.18;
+	finalPrice = price - discount;
+
+	cout << "The final price is: " << finalPrice << " lv." << endl;
+	cout << "The discount is: " << discount << " lv." << endl;
+	return 0;
+}
+
