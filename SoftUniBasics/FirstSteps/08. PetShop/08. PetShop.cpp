a040110216f9a944564fcd91b3805b091d70038e
Gergana Ivanova <geri7423@abv.bg>
2025-05-02 13:18:05 +0300

    solution for task 8

---
+++ SoftUniBasics/FirstSteps/08. PetShop/08. PetShop.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	double dogFood, catFood, price;
+
+	cin >> dogFood;
+	cin >> catFood;
+
+	price = dogFood * 2.5 + catFood * 4;
+
+	cout << price << " lv." << endl;
+	return 0;
+}
