78f113c2c1ffd7bde55843d051c5a54595802b93
Gergana Ivanova <geri7423@abv.bg>
2025-05-02 12:54:56 +0300

    solution for task 4

---
+++ SoftUniBasics/FirstSteps/04.InchesToCentimeters/04.InchesToCentimeters.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double number, result;
+    cin >> number;
+
+    result = number * 2.54;
+
+    cout << result << endl;
+    return 0;
+}
