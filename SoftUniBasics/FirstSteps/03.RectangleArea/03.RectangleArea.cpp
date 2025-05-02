61b9a2589ebe250be35648d9fd5b07ad7ea742ba
Gergana Ivanova <geri7423@abv.bg>
2025-05-02 12:36:15 +0300

    solution for task 3

---
+++ SoftUniBasics/FirstSteps/03.RectangleArea/03.RectangleArea.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, area;
+    cout << "Enter side a: ";
+    cin >> a;
+    cout << "Enter side b: ";
+    cin >> b;
+
+    area = a * b;
+    cout << "Rectangle area is: " << area << endl;
+}
