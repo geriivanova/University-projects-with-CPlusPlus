83e9d5699a36158dfa8d46d0ed869d67d147a380
Gergana Ivanova <geri7423@abv.bg>
2025-05-03 16:29:40 +0300

    solution for task 3

---
+++ SoftUniBasics/First Steps In Coding - Exercise/03.Calculator/03.Calculator.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double depositeSum, persentage, sum;
+    int depositeTerm;
+    cin >> depositeSum;
+    cin >> depositeTerm;
+    cin >> persentage;
+
+    sum = depositeSum + depositeTerm * ((depositeSum * persentage / 100) / 12);
+
+    cout << sum << endl;
+    return 0;
+}
+
