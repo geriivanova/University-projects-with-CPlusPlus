41b284ca722b9844d242e918c8ea3d73aecb9a14
Gergana Ivanova <geri7423@abv.bg>
2025-05-03 18:13:11 +0300

    solution for task 4

---
+++ SoftUniBasics/First Steps In Coding - Exercise/04. Vacation/04. Vacation.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int pagesCount, pagesPerHour, days, result;
+
+    cin >> pagesCount;
+    cin >> pagesPerHour;
+    cin >> days;
+
+    result = ((pagesCount / pagesPerHour)) / days;
+    cout << result << endl;
+    return 0;
+}
