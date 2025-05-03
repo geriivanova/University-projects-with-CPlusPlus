afcfcc19c0d62213ccbd4da5d5e9fe3f1f3b7f19
Gergana Ivanova <geri7423@abv.bg>
2025-05-03 18:23:38 +0300

    solution for task 5

---
+++ SoftUniBasics/First Steps In Coding - Exercise/05. Supplies/05. Supplies.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int penPackages, markerPackages, boardCleaner, discount;
+    double penPrice, markerPrice, cleanerPrice, price, priceWithDiscount;
+
+    cin >> penPackages >> markerPackages >> boardCleaner >> discount;
+
+    penPrice = penPackages * 5.80;
+    markerPrice = markerPackages * 7.20;
+    cleanerPrice = boardCleaner * 1.20;
+
+    price = penPrice + markerPrice + cleanerPrice;
+
+    priceWithDiscount = price - (price * discount / 100);
+    cout << priceWithDiscount << endl;
+    return 0;
+}
