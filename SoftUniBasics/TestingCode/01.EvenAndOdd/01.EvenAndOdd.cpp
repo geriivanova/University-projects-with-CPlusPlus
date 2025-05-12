89f66fd7d094a9aeabf0167ead1a416b029e12a3
Gergana Ivanova <geri7423@abv.bg>
2025-05-12 19:49:12 +0300

    task1 solution

---
+++ SoftUniBasics/TestingCode/01.EvenAndOdd/01.EvenAndOdd.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+void evenOrOdd(int array[], int size)
+{
+    int countEven = 0;
+    int countOdd = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            countEven++;
+        }
+        else
+        {
+            countOdd++;
+        }
+    }
+
+    cout << "Count of even numbers is: " << countEven << '\n';
+    cout << "Count of odd numbers is: " << countOdd << '\n';
+}
+
+int main()
+{
+    int arr[] = { 1, 3, 7, 8, 9, 42, 16, 4, 1 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    evenOrOdd(arr, size);
+    return 0;
+}
