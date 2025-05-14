e84f327e504506e25c524b97287d37d554846506
Gergana Ivanova <geri7423@abv.bg>
2025-05-14 23:23:49 +0300

    task 8 solution

---
+++ SoftUniBasics/TestingCode/08.Enumerations/08.Enumerations.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+enum Level {
+    LOW = 0,
+    MEDIUM = 1,
+    HIGH = 2
+};
+int main()
+{
+    enum Level myVar = MEDIUM;
+    cout << myVar;
+    return 0;
+}
