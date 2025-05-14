1aef22a3d3003f41a08e241793d0ac62a3438121
Gergana Ivanova <geri7423@abv.bg>
2025-05-14 16:29:26 +0300

    task 5 solution

---
+++ SoftUniBasics/TestingCode/05.CinIgnore/05.CinIgnore.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int num;
+    string line;
+
+    cin >> num;
+    cin.ignore();
+    getline(cin, line);
+
+    cout << "Число: " << num << "\nТекст: " << line << endl;
+    return 0;
+}
