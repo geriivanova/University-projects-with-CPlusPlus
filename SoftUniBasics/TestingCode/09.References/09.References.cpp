aada2be63ea0c28b88ecd6125d25c34d51c2d6b9
Gergana Ivanova <geri7423@abv.bg>
2025-05-14 23:46:22 +0300

    task 9 and 10

---
+++ SoftUniBasics/TestingCode/09.References/09.References.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    string food = "Pizza";
+    string &meal = food;
+
+    cout << food << "\n";  
+    cout << meal << "\n";
+
+    string food1 = "Pizza";
+    cout << &food1; //memory address
+
+    return 0;
+}
---
+++ SoftUniBasics/TestingCode/10.Pointers/10.Pointers.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    string food = "Pizza"; 
+    string* ptr = &food;   
+
+    cout << food << "\n";
+
+    cout << &food << "\n";
+
+    cout << ptr << "\n";
+    return 0;
+}
+
