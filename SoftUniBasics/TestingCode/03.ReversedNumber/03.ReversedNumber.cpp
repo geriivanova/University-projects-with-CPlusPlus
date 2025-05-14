c8a7d7d671fe9eed4efbb7c230523a400c3ea845
Gergana Ivanova <geri7423@abv.bg>
2025-05-14 15:19:52 +0300

    task 3 solution

---
+++ SoftUniBasics/TestingCode/03.ReversedNumber/03.ReversedNumber.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int number, reversedNumber = 0;
+    cin >> number;
+
+    while (number)
+    {
+        reversedNumber = reversedNumber * 10 + number % 10;
+        number /= 10;
+    }
+
+    cout << "Reversed number is: " << reversedNumber << "\n";
+    return 0;
+}
