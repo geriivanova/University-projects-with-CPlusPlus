9086de79ce9e25039dcb8694743b43b177a6eeaa
Gergana Ivanova <geri7423@abv.bg>
2025-05-15 14:58:55 +0300

    task 2

---
+++ SoftUniBasics/Arrays/02.Queue/02.Queue.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue<string> cars;
+
+    cars.push("Volvo");
+    cars.push("BMW");
+    cars.push("Ford");
+
+    cout << cars.size();
+    return 0;
+}
