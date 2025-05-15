30042fddf60ae441fcdda1a81ea8ca5a48adc67d
Gergana Ivanova <geri7423@abv.bg>
2025-05-15 15:21:08 +0300

    task 3

---
+++ SoftUniBasics/Arrays/03.FibonaciNumbers/03.FibonaciNumbers.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int fibonaciNumbers(int n) {
+    if (n <= 1)
+        return n;
+    return fibonaciNumbers(n - 1) + fibonaciNumbers(n - 2);
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibonaciNumbers(n);
+    return 0;
+}
